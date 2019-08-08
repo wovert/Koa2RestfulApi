67f8ea44b245be9c172c8f268be1f3ea3a2ab4eb
wovert <67668283@qq.com>
2019-08-08 13:50:28 +0800

    require-directory module

--- island-api/src/app.js
+++ island-api/src/app.js
@@ -1,14 +1,22 @@
 const Koa = require('koa')
-const book = require('./api/v1/book')
-const classic = require('./api/v1/classic')
+const Router = require('koa-router')
+const rd = require('require-directory')
+
 const port = 3000
 const app = new Koa()
 
-//////////////////////////////////////////////////////
-//////////// Register router /////////////////////////
-app.use(book.routes())
-app.use(classic.routes())
-//////////////////////////////////////////////////////
+// const book = require('./api/v1/book')
+// const classic = require('./api/v1/classic')
+// app.use(book.routes())
+// app.use(classic.routes()) 
+
+rd(module, './api', {
+  visit: function (obj) {
+    if (obj instanceof Router) {
+      app.use(obj.routes())
+    }
+  }
+})
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`)
